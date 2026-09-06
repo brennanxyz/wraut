2579262e90447359411656838a70777c02f5f0d2
brennanxyz <brennan@brennanharris.xyz>
2026-09-05 21:07:11 -0600

    Add missing table column

--- src/modules/service/html.rs
+++ src/modules/service/html.rs
@@ -25,6 +25,7 @@ pub fn list(
                             <th>Repo</th>
                             <th>URL</th>
                             <th>Active</th>
+                            <th>Key</th>
                             <th>Status</th>
                             <th style=\"display:flex; justify-content:center;\">Actions</th>
                         </tr>
@@ -34,6 +35,7 @@ pub fn list(
                                 format!(
                                     "
                             <tr>
+                                <td>{}</td>
                                 <td>{}</td>
                                 <td>{}</td>
                                 <td>{}</td>
@@ -71,6 +73,7 @@ pub fn list(
                                     dbe.repo_url,
                                     dbe.access_url,
                                     dbe.active,
+                                    dbe.use_key,
                                     dbe.id,
                                     match dbe.is_running(&dkl) {
                                         true => "success",
